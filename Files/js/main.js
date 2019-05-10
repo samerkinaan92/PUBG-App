3f09f89c5f76b0ce0ea7d7f4c6450a283c09af7a
samer kinaan <samerkinaan@gmail.com>
2019-05-11 02:17:03 +0300

    limit matches to 50

--- Files/js/main.js
+++ Files/js/main.js
@@ -20,7 +20,7 @@ var mapNames = {
     "Savage_Main": "Sanhok"
 };
 
-var events = ["kill", "knockout", "headshot", "revived", "knockedout", "death", "win", "jump", "fire", "damageTaken"];
+var events = {"kill": "Kill", "knockout": "Knockout", "headshot": "Headshot", "revived": "Revived", "knockedout": "Knockedout", "death": "Death", "win": "Win", "jump": "Jump", "fire": "Fire", "damageTaken": "Taken damage"};
 var logDev = [];
 
 var windowId;
@@ -32,6 +32,8 @@ var isFeatureSet = false;
 var voicesIds= {};
 var voicesPaths = {};
 var leds = {};
+var playersAlert = [];
+var matchesNum = 0;
 
 var mySwiper = new Swiper('.swiper-container', {
   speed: 400,
@@ -43,12 +45,17 @@ var mySwiper = new Swiper('.swiper-container', {
   spaceBetween: 3,
   // Responsive breakpoints
   breakpoints: {
-    // when window width is <= 480px
-    1000: {
+    // when window width is <= 1000px
+    600: {
       slidesPerView: 1,
       spaceBetween: 3,
     },
-    // when window width is <= 640px
+    // when window width is <= 1000px
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 3,
+    },
+    // when window width is <= 1350px
     1350: {
       slidesPerView: 3,
       spaceBetween: 3,
@@ -95,14 +102,22 @@ overwolf.windows.getCurrentWindow(function(result) {
 overwolf.settings.registerHotKey(
   "stop_playback",
   function(arg) {
-    console.log(arg);
-      if (arg.status == "success") {
-          overwolf.media.audio.stop(function(res){
-            console.log(res);
-          });
-      }
+    if (arg.status == "success") {
+        overwolf.media.audio.stop(function(res){
+      });
+    }
   }
 );
+
+overwolf.settings.getHotKey("stop_playback", function(arg){
+  if(arg.status == "success"){
+    $("#hotkey").html('"' + arg.hotkey + '"');
+  }
+});
+
+overwolf.settings.OnHotKeyChanged.addListener(function(res){
+  $("#hotkey").html('"' + res.hotkey + '"');
+});
 
 
 $("#live-stats-table").hide();
@@ -119,9 +134,6 @@ overwolf.media.audio.onPlayStateChanged.addListener(function(res){
     if(voicesIds.hasOwnProperty(voice)){
       if(voicesIds[voice] == res.id){
         if(res.playback_state == "playing"){
-          overwolf.media.audio.setVolume(0, function(res){
-            console.log(res);
-          });
           $("#" + voice + "VoicePlayBtn").html('<img src="img/svg/speakerOff.svg">');
           $("#" + voice + "VoicePlayBtn").val(1);
         }else if(res.playback_state == "stopped"){
@@ -152,11 +164,11 @@ function registerEvents() {
     // This will also be triggered the first time we register
     // for events and will contain all the current information
     overwolf.games.events.onInfoUpdates2.addListener(function(info) {
-      console.log("Info UPDATE: " + JSON.stringify(info));
       if("match_info" in info.info){
         //info update about match
         var match_info = info.info.match_info;
-        match_info_update(match_info);
+        var feature = info.feature;
+        match_info_update(match_info, feature);
       }
       if("game_info" in info.info){
         //info update about game
@@ -168,7 +180,7 @@ function registerEvents() {
     // an event triggerd
     overwolf.games.events.onNewEvents.addListener(function(info) {
       console.log("EVENT FIRED: " + JSON.stringify(info));
-      playVoice(info.events[0].name);
+      playVoice(info.events[0].name, 0);
       playLed(info.events[0].name);
       if(info.events[0].name === "damage_dealt"){
         damage_dealt_event(info.events[0].data);
@@ -273,7 +285,7 @@ function setFeatures() {
 }
 
 //updates for match
-function match_info_update(match_info){
+function match_info_update(match_info, feature){
   if("map" in match_info){
     $("#map").html("map: " + mapNames[match_info.map]);
   }else if("kills" in match_info){
@@ -295,6 +307,7 @@ function match_info_update(match_info){
     var data = JSON.parse(match_info.nicknames);
     if("team_members" in data){
       team = data.team_members;
+      console.log(Object.prototype.toString.call(team));
       $("#team").html("team: " + data.team_members);
     }
   }else if("total_teams" in match_info){
@@ -308,8 +321,8 @@ function match_info_update(match_info){
     if(match.rank_me === "1"){
       playVoice('win');
     }
-  }else if("roster" in match_info){
-
+  }else if(feature === "roster"){
+    chkPlyrAlrt(match_info);
   }
 }
 
@@ -457,12 +470,16 @@ function hideSession(){
 //adds match to swiper and saves in DB
 function addMatch(){
   match.date = getDateString();
-  let slides = mySwiper.slides.length + 1;
+  matchesNum++;
 
-  match.matchName = "Match " + (slides.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})); 
+  match.matchName = "Match " + (matchesNum.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}));
+  if(matchesNum > 50){
+    matches.shift();
+    mySwiper.removeSlide(0);
+  }
   appendSlide(match);
   matches.push(match);
-  mySwiper.slideNext()
+  mySwiper.slideNext();
   saveSessionFile();
 }
 
@@ -484,6 +501,8 @@ function resetLiveFeed(){
   $("#knockedouts").html("knockedouts: 0");
   $("#knockouts").html("knockouts:");
   $("#killer").html("killer:");
+  $("#playerInMatch").empty();
+  $("#live-stats-team-table").empty();
 }
 
 
@@ -495,7 +514,7 @@ function showMatchSession(index){
       <h2>` + matches[index].matchName + `</h2>
       <div class="session-controls-group">
         <button class="session-control" onclick="hideSession()">
-          <img class="svg-icon-fill" src="img/svg/close.svg" width="10", height="10">
+          <img class="svg-icon-fill" src="img/svg/closeB.svg" width="10", height="10">
         </button>
       </div>`);
   $(".selected-session-div").append(`
@@ -525,13 +544,21 @@ function showMatchSession(index){
   if(matches[index].mode === "solo"){
     $("#old-session-team").hide();
     $("#old-session-knock").hide();
+  }else if(matches[index].team_kills !== undefined){
+    $(".selected-session-div").append(`
+      <table style="width:100%">
+      ` + matches[index].team_kills + `
+      </table>
+    `);
   }
 }
 
 //saves match to local DB
 function saveSessionFile(){
-  let json = JSON.stringify(matches);
+  var json = JSON.stringify(matches);
   localStorage.matches = json;
+  json = JSON.stringify(matchesNum);
+  localStorage.matchesNum = json;
 }
 
 //load all matches from local DB
@@ -543,6 +570,7 @@ function loadSessionFile(){
       appendSlide(element);
     });
     mySwiper.slideTo(matches.length - 1);
+    matchesNum = JSON.parse(localStorage.matchesNum);
     console.log("matches been loaded");
   }else{
     console.log("matches is null");
@@ -593,6 +621,15 @@ function loadSettings(){
     console.log("voices has been loaded");
   }else{
     console.log("no settings where found");
+  }
+
+  if(localStorage.playersAlert){
+    playersAlert = JSON.parse(localStorage.playersAlert);
+    playersAlert.forEach(function(element){
+      $("#playerList").append(`
+        <option value="` + element + `">` + element + `</option>
+      `);
+    });
   }
 
   setTimeout(loadFlashLed, 200);
@@ -653,17 +690,20 @@ function playVoice(event, val){
   if (voicesIds[event] === undefined){
     console.log(event + " voice is undefined");
   }else{
-    var vol = parseInt($("#" + event + "Volume").val());
     if(val == 0){
       overwolf.media.audio.play(voicesIds[event], function(res){
         if(res.status === "success"){
           console.log(event + " voice is playing");
+        }else{
+          console.log(event + " voice is playing is not successful!");
         }
       });
     }else if(val == 1){
       overwolf.media.audio.stopById(voicesIds[event], function(res){
         if(res.status === "success"){
           console.log(event + " voice stopped");
+        }else{
+          console.log(event + " voice is stop is not successful!");
         }
       });
     }
@@ -672,24 +712,22 @@ function playVoice(event, val){
 
 function initSelect(str){
   var select = $("#"+ str + "-event-select");
-  events.forEach(element => {
-    var option = $('<option></option>');
-    option.attr('value', element);
-    option.text(element);
-    select.append(option);
-  });
+  for(var property in events){
+    if (events.hasOwnProperty(property)) {
+      var option = $('<option></option>');
+      option.attr('value', property);
+      option.text(events[property]);
+      select.append(option);
+    }
+  }
 }
 
 function addVoiceEvent(selected){
   $("#voice-table").append(`
   <tr id="` + selected + `-voice-event">
-    <th scope="row">` + selected + `</th>
+    <th scope="row">` + events[selected] + `</th>
     <td>
       <button class="settings-btns" onclick="getVoice('` + selected + `', null)">Choose...</button>
-    </td>
-    <td>
-      <input type="range" id="` + selected + `Volume" min="0" max="100" onchange="setVol('` + selected + `')">
-      <span id="` + selected + `ValBox">50</span>
     </td>
     <td>
       <button value="0" id="` + selected + `VoicePlayBtn" class="session-control" onclick="playVoice('` + selected + `', this.value)"><img src="img/svg/speaker.svg"></button>
@@ -701,14 +739,6 @@ function addVoiceEvent(selected){
   `);
 
   $("#voice-event-select option[value='" + selected + "']").remove();
-}
-
-function setVol(selected){
-  var vol = parseInt($("#" + selected + "Volume").val());
-  $("#"  + selected + "ValBox").text(vol);
-  overwolf.media.audio.setVolumeById(voicesIds[selected] ,vol, function(res){
-    console.log(res);
-  });
 }
 
 function delVoice(event){
@@ -726,7 +756,7 @@ function delVoice(event){
       var select = $("#voice-event-select");
       var option = $('<option></option>');
       option.attr('value', event);
-      option.text(event);
+      option.text(events[event]);
       select.append(option);
       saveVoices();
     }
@@ -736,7 +766,7 @@ function delVoice(event){
 function addLEDEvent(selected, prev){
   $("#RGB-table").append(`
   <tr id="` + selected + `-led-event">
-    <th>` + selected + `</th>
+    <th>` + events[selected] + `</th>
     <td>
       <!-- preview element -->
       <div class="preview" id="` + selected + `preview"></div>
@@ -862,7 +892,7 @@ function playLed(selected){
   var dur = $("#" + selected + "LedDur").val();
   var int = $("#" + selected + "LedInt").val();
   if(!col || !dur || !int){
-    console.log("event not defined good");
+    console.log(selected + " LED event is not defined");
   }else{
     flashLighting(col, dur, int);
   }
@@ -881,7 +911,7 @@ function delLED(selected){
       var select = $("#RGB-event-select");
       var option = $('<option></option>');
       option.attr('value', selected);
-      option.text(selected);
+      option.text(events[selected]);
       select.append(option);
       delete leds[selected];
       saveLED();
@@ -897,7 +927,6 @@ function saveLED(){
 function flashLighting(color, duration, interval){
   var rgbColor = convertColor(color);
   overwolf.logitech.led.flashLighting(rgbColor.rChannel, rgbColor.gChannel, rgbColor.bChannel, duration, interval, function(res){
-    console.log(res);
   });
 }
 
@@ -919,9 +948,6 @@ function convertColor(color) {
  }
 
 function getLEDSyncDev(){
-  overwolf.logitech.getVersion(function(res){
-  });
-
   overwolf.logitech.getDevices(function(res){
     logDev = res.devices;
     var isRGB = false;
@@ -932,28 +958,32 @@ function getLEDSyncDev(){
     });
     if(isRGB){
       $(".logitecDevLst").append(`
+        <p>Here you can set Logitech devices to flash on event, for example you can set flash effects that will play each time you get a kill or get a win.<br><br></p>
+        <table class="table">
+          <tr>
+            <td>
+              <span>Event: </span><select id="RGB-event-select"></select>
+            </td>
+            <td>
+              <button class="settings-btns tooltip" onclick="addLEDEvent($('#RGB-event-select').val(), false)">ADD<span class="tooltiptext">Add LED effect</span></button>
+            </td>
+          </tr>
+        </table>
         <table id="RGB-table" class="table">
           <thead class="thead-dark">
             <tr>
-              <td>
-                <select id="RGB-event-select"></select>
-              </td>
-              <td>
-                <button class="settings-btns" onclick="addLEDEvent($('#RGB-event-select').val(), false)">ADD</button>
-              </td>
-            </tr>
-            <tr>
               <th>Event</th>
-              <th>Color</th>
-              <th>Duration</th>
-              <th>Interval</th>
-              <th>Test</th>
+              <th class="tooltip">Color<span class="tooltiptext">Set LED color effect</span></th>
+              <th class="tooltip">Duration<span class="tooltiptext">Set the duration time LED effect will play</span></th>
+              <th class="tooltip">Interval<span class="tooltiptext">Set the interval time flash will play</span></th>
+              <th class="tooltip">Test<span class="tooltiptext">Test LED effect</span></th>
               <th> </th>
             </tr>
           </thead>
         </table>
       `);
       initSelect("RGB");
+      
       overwolf.logitech.led.init(function(res){
       });
     
@@ -971,7 +1001,79 @@ function getLEDSyncDev(){
 }
 
 function addPlayer(name){
-  $("#playerList").append(`
-    <option value="` + name + `">` + name + `</option>
-  `);
+  if(name.length >= 3){
+    $("#playerList").append(`
+      <option value="` + name + `">` + name + `</option>
+    `);
+    playersAlert.push(name);
+    $("#playerTxt").val('');
+    var json = JSON.stringify(playersAlert);
+    localStorage.playersAlert = json;
+    console.log(name + " has been added to alert list");
+  }
+}
+
+function removePlayer(){
+  $.each($("#playerList option:selected"), function(){
+    $("#playerList option[value='" + $(this).val() + "']").remove();
+    for( var i = 0; i < playersAlert.length; i++){ 
+      if ( playersAlert[i] === $(this).val()) {
+        playersAlert.splice(i, 1); 
+        i--;
+      }
+    }
+    console.log($(this).val() + " has been added to alert list");
+    var json = JSON.stringify(playersAlert);
+    localStorage.playersAlert = json;
+  });
+}
+
+function chkPlyrAlrt(roster){
+  var names = Object.getOwnPropertyNames(roster);
+  var playerRos = JSON.parse(roster[names[0]]);
+  if("player" in playerRos){
+    var playerName = playerRos["player"];
+    if(match.mode !== "solo"){
+        if(playerRos["out"]){
+        team.forEach(function(element){
+          if(element === playerName){
+            $("#live-stats-team-table").append("<tr><td>" + playerName + " has " + playerRos["kills"] + " kills</td></tr>");
+            match.team_kills = $("#live-stats-team-table").html();
+          }
+        });
+      }
+    }
+    var lowercaseName = playerName.toLowerCase();
+    playersAlert.forEach(function(element){
+      if(lowercaseName.includes(element.toLowerCase())){
+        if(!playerRos["out"]){
+          console.log(playerName + " is in match");
+          $("#playerInMatch").append(`
+            <option value="` + playerName + `In">` + playerName + " is in match" + `</option>
+          `);
+          $("#playerInMatch").val(playerName + 'In');
+        }else{
+          console.log(playerName + " has died with " + playerRos["kills"] + " kills");
+          $("#playerInMatch").append(`
+            <option value="` + playerName + `Out">` + playerName + " has died with " + playerRos["kills"] + " kills" + `</option>
+          `);
+          $("#playerInMatch").val(playerName + 'Out');
+        }
+      }
+    });
+  }
+}
+
+function openSetting(evt, SettingName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(SettingName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
